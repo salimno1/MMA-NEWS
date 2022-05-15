9fecc886e2460d47dfd4330ea6702ae03f8c1683
Salim Mvoungui <salim.mvoungui@gmail.com>
2022-05-16 00:23:56 +0200

    okej

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,40 +1,27 @@
 import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-05/nba-plain--1bf677e2-987b-4765-9879-c2183d8e1a63.jpeg?itok=ixWgVxa0"
-        alt=""
-      />
+      {post.photo && <img className="postImg" src="{post.photo}" alt="" />}
+
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Crypto</span>
+          {post.categories.map((c) => {
+            <span className="postCat">{c.name}</span>;
+          })}
         </div>
-        <span className="postTitle">
-          Lorem ipsum, dolor sit amet consectetur.
-        </span>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+
         <hr />
-        <span className="postDate"> 1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus a
-        inventore soluta hic, harum reiciendis aut voluptatum tempore fuga
-        veniam explicabo dolorum eaque praesentium distinctio saepe, accusantium
-        enim autem quas. Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Temporibus a inventore soluta hic, harum reiciendis aut voluptatum
-        tempore fuga veniam explicabo dolorum eaque praesentium distinctio
-        saepe, accusantium enim autem quas. Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Temporibus a inventore soluta hic, harum
-        reiciendis aut voluptatum tempore fuga veniam explicabo dolorum eaque
-        praesentium distinctio saepe, accusantium enim autem quas. Lorem ipsum
-        dolor sit amet consectetur, adipisicing elit. Temporibus a inventore
-        soluta hic, harum reiciendis aut voluptatum tempore fuga veniam
-        explicabo dolorum eaque praesentium distinctio saepe, accusantium enim
-        autem quas.
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/siderbar/Sidebar";
 import "./home.css";
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+      console.log(res);
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
